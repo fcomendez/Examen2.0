92cc22b7ee88cf46e24353d10a9bdc5d29752e89
ByronUlloa <101208945+ByronUlloa@users.noreply.github.com>
2022-06-17 02:27:59 -0400

    productos y modelos de productos

---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import productos, carrito
+
+urlpatterns = [
+    path('', productos, name="productos"),
+    #path('', carrito, name="carrito"),
+]
---
+++ app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import redirect, render
+from .models import Producto
+# Create your views here.
+
+def productos(request):
+    productos = Producto.objects.all()
+    data = {
+        'productos': productos
+    }
+    return render(request, 'app/productos.html', data)
+
+def carrito(request):
+    return render(request, 'app/carrito.html')
